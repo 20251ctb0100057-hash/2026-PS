93843fd53ce1c1b10def8e15493d8929c3462ee6
20251ctb0100057-hash <20251ctb0100057@estudantes.ifpr.edu.br>
2026-02-19 17:34:15 +0000

    add calculadora

---
+++ 02_calculadora/main.py
@@ -0,0 +1,26 @@
+def soma(a, b): return a + b
+def subtracao(a, b): return a - b
+def multiplicacao(a, b): return a * b
+def divisao(a, b):
+    if b == 0:
+        return "Erro (divisão por zero)"
+    return a / b
+
+def calculadora():
+    try:
+        
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+
+        
+        print("\n Resultado")
+        print(f"Soma: {soma(num1, num2)}")
+        print(f"Subtração: {subtracao(num1, num2)}")
+        print(f"Multiplicação: {multiplicacao(num1, num2)}")
+        print(f"Divisão: {divisao(num1, num2)}")
+        
+    except ValueError:
+        print("Erro: Por favor, digite apenas números válidos (use ponto em vez de vírgula).")
+
+if __name__ == "__main__":
+    calculadora()
