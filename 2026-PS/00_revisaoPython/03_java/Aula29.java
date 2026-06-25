6f32cad8a06103b38d09e243926d8675935a2213
20251ctb0100057-hash <20251ctb0100057@estudantes.ifpr.edu.br>
2026-06-25 15:18:12 +0000

    Aula29

---
+++ 2026-PS/00_revisaoPython/03_java/Aula29.java
@@ -0,0 +1,65 @@
+// Media da turma //
+public class Aula29 {public static double calcularMedia(double[] notas){
+    double soma = 0;
+    for (double nota : notas) {
+        soma += nota;
+    }
+            return soma /
+            notas.length;
+            }
+}
+
+// Contar Aprovados //
+
+    public static int
+    contarAprovados(double[] notas)
+    {
+        int aprovados = 0;
+        for (double nota: notas) {
+        }
+        if (nota >= 6.0) {
+            aprovados++;
+        }
+        return aprovados;
+    }
+    
+// Catálogo dos Produtos ArrayList//
+import java.util.ArrayList;
+
+        public static void adicionarProduto(ArrayList<String> lista, String nome) {
+            lista.add(nome);
+        }
+        public static void listarProdutos(ArrayList<String>lista) {
+            for (int i=0; i < lista.size(); i++) {
+                System.out.println((i+1) + " - " + lista.get(i));
+            }
+        }
+
+// Maior Valor com sobreacraga //
+
+public static int maiorValor(int[] valores) {
+    int maior = valores[0];
+    for (int valor: valores) {
+        if (valor > maior) {
+            maior = valor;
+        }
+    }
+    return maior;
+}
+
+public static int maiorValor(int a, int b) {
+    return Math.max(a,b);
+}
+
+// Boletim com IntegradoR //
+
+    public static void exibirBoletim(double[] notas) {
+        double media = calcularMedia(notas);
+        int aprovados = contarAprovados(notas);
+        String situacao = (media >= 6.0) ? "APROVADA": "EM RECUPERAÇÃO";
+
+        System.out.printf("Média: %.2f%n", media);
+        System.out.println("Aprovados: " + aprovados);
+        System.out.println("Situação: " + situacao);
+
+    }
