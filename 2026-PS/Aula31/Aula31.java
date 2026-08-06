84e26c0bf64e671f53ea4cabac2d5eae491b0ee7
20251ctb0100057-hash <20251ctb0100057@estudantes.ifpr.edu.br>
2026-08-06 14:56:08 +0000

    Aula31

---
+++ 2026-PS/Aula31/Aula31.java
@@ -0,0 +1,72 @@
+/*Yuri Gonçalves Leuch
+Exercicios com java: Menor valor, Maior valor, Contar acima e Soma - Float, int, double
+*/
+    // 1 - Calcula Soma
+
+    int calculaSoma(int[] num) {
+
+        int soma = 0;
+
+        for (int n : num) {
+            soma += n;
+        }
+
+        return soma;
+    }
+
+    // 2 - Calcula Média
+
+     int calculaMedia(int[] num) {
+
+        int soma = 0;
+
+        for (int n : num) {
+            soma += n;
+        }
+
+        return soma / num.length;
+    }
+
+    // 3 - Menor Valor
+    int menorValor(int[] num) {
+
+        int menor = num[0];
+
+        for (int n : num) {
+            if (n < menor) {
+                menor = n;
+            }
+        }
+
+        return menor;
+    }
+
+    // 4 - Maior Valor
+    int maiorValor(int[] num) {
+
+        int maior = num[0];
+
+        for (int n : num) {
+            if (n > maior) {
+                maior = n;
+            }
+        }
+
+        return maior;
+    }
+
+    // 5 - Contar Acima
+    int contarAcima(int[] num, int limite) {
+
+        int contador = 0;
+
+        for (int n : num) {
+            if (n > limite) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+
+}
